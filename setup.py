973398ca53aa83148545f178dca904a35ed65478
Marika <marika@memmu.(none)>
2011-07-09 13:44:55 +0300

    Added stub for checking and installing required modules, and high enough python version.

--- setup.py
+++ setup.py
@@ -1,6 +1,36 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from distutils.command.install import install
+import sys
+
+class CustomInstall(install):
+    def run(self):
+        print "Doing custom install."
+        if not checkPythonVersion():
+            print "ERROR: Wrong python version!"
+            pass
+             # TODO: install python 2.7.1
+        if not checkDependencies():
+            print "ERROR: Missing some python modules!"
+            pass
+             # TODO: install missing modules
+        install.run(self) # proceed with the installation
+
+def checkPythonVersion():
+    #TODO: this should not pass
+    return sys.version_info >= (2, 7, 1) #TODO: does e.g python 3 work?
+
+def checkDependencies():
+    try:
+        __import__('Tkinter')
+        __import__('argparse')
+        __import__('Mock')
+        return True
+
+    except ImportError:
+        return None
+
 
 long_description = 'Slaps your desired apps on your screen in your desired style'
 
@@ -12,6 +42,7 @@ setup(name = 'appslap',
         url = 'http://www.github.com/appSlap', #TODO: define url
         long_description = long_description,
         packages = ['appslap'],
-        scripts=['bin/appslap']
+        scripts=['bin/appslap'],
+        cmdclass=dict(install=CustomInstall)
         )
 
